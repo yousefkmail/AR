8ecc319788f3765b4c48c1a8c0798777eb6dc6ef
Yousef Ali <yousefkmail169@gmail.com>
2024-10-15 00:42:52 +0300

    feat:add ar

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,28 @@
-import React, { useEffect } from "react";
-import { Canvas, useThree } from "@react-three/fiber";
-import { ARButton } from "three/examples/jsm/webxr/ARButton";
+import { Canvas } from "@react-three/fiber";
+import { XR, createXRStore } from "@react-three/xr";
+import { useState } from "react";
 
-navigator.xr.isSessionSupported("immersive-ar").then((supported) => {
-  if (supported) {
-    console.log("AR is supported");
-  } else {
-    console.log("AR is not supported on this device");
-  }
-});
+const store = createXRStore();
 
-const ARScene: React.FC = () => {
-  const { gl } = useThree();
-
-  useEffect(() => {
-    // Enable WebXR in the renderer
-    gl.xr.enabled = true;
-
-    // Create the AR button and append to the document
-    const arButton = ARButton.createButton(gl);
-    document.body.appendChild(arButton);
-  }, [gl]);
-
+function App() {
+  const [red, setRed] = useState(false);
   return (
     <>
-      <ambientLight intensity={0.5} />
-      <directionalLight position={[5, 5, 5]} intensity={1} />
-      <mesh position={[0, 0.5, 0]}>
-        <boxGeometry args={[1, 1, 1]} />
-        <meshStandardMaterial color="orange" />
-      </mesh>
+      <button onClick={() => store.enterAR()}>Enter AR</button>
+      <Canvas>
+        <XR store={store}>
+          <mesh
+            pointerEventsType={{ deny: "grab" }}
+            onClick={() => setRed(!red)}
+            position={[0, 1, -1]}
+          >
+            <boxGeometry />
+            <meshBasicMaterial color={red ? "red" : "blue"} />
+          </mesh>
+        </XR>
+      </Canvas>
     </>
   );
-};
-
-const App: React.FC = () => {
-  return (
-    <Canvas
-      style={{
-        height: "100vh",
-        width: "100vw",
-        position: "absolute",
-        top: 0,
-        left: 0,
-      }}
-      camera={{ position: [0, 1.6, 3] }} // Typical camera position for AR
-    >
-      <ARScene />
-    </Canvas>
-  );
-};
+}
 
 export default App;
