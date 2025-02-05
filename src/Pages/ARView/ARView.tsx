0a688cc38659a817d08e24e8ebb8439729dabaec
Yousef Ali <yousefkmail169@gmail.com>
2025-02-05 11:58:38 +0200

    feat: add simple AR scene

--- src/Pages/ARView/ARView.tsx
+++ src/Pages/ARView/ARView.tsx
@@ -1,15 +1,13 @@
 import { Canvas } from "@react-three/fiber";
 import { XR, createXRStore } from "@react-three/xr";
-import { useEffect } from "react";
 
 export default function ARView() {
   const store = createXRStore();
 
-  useEffect(() => {
-    store.enterAR();
-  }, []);
   return (
     <div style={{ height: "100%", position: "relative", overflow: "hidden" }}>
+      <button onClick={() => store.enterAR()}>Enter AR</button>
+
       <Canvas>
         <XR store={store}>
           <mesh pointerEventsType={{ deny: "grab" }} position={[0, 1, -1]}>
